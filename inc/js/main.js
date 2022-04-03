e51d509062e81176bdfa6a49d8d37def61af2b5d
Alexey Saulin <60825554+Lion116@users.noreply.github.com>
2022-04-03 18:29:12 +0300

    Add files via upload

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,4 +1,4 @@
-import {questLang} from "./questLang.js";
+// import {questLang} from "./questLang.js";
 
 const blockBtn = document.querySelectorAll(".monBlock");
 const block = document.querySelectorAll(".block");
@@ -9,16 +9,22 @@ const textQuest = document.querySelector(".TextQuest");
 const codeQuest = document.querySelector(".CodeQuest");
 const monitor = document.querySelector("#monitor");
 const blockMon = document.querySelector(".blockMon");
+const sel = document.querySelector("#sel");
+const codeBlock = document.querySelector(".CodeBlock");
+const subCode = document.querySelector(".SubCode");
 
 const deactivation = () => {
 	monitor.classList.remove('open');
 	blockMon.classList.remove('hidden');
+	codeBlock.classList.remove('hidden');
+	subCode.classList.remove('hidden');
 	navMon.forEach(item => item.classList.remove('open'));
 	menu.forEach(item => item.classList.remove('open'));
 }
 
 blockBtn.forEach((btn,i)=>btn.addEventListener('click', () => {
 	deactivation();
+	sel.style.display = 'block';
 	nameQuest.textContent = block[i].textContent;
 	let quest = questLang(block[i].textContent);
 	textQuest.textContent = quest[0];
@@ -26,4 +32,6 @@ blockBtn.forEach((btn,i)=>btn.addEventListener('click', () => {
 		blockMon.classList.add('hidden');
 	else
 		codeQuest.textContent = quest[1];
+	monitor.style.display = 'none';
+	sel.value = block[i].textContent;
 }));
