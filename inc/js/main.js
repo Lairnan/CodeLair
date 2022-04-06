86a8e486d04c6d7d33cd4dd198ee5d2d487eb83f
Alexey Saulin <alexey.saulin03@mail.ru>
2022-04-06 23:19:36 +0300

    Some little fix and add func for button submit

--- inc/js/main.js
+++ inc/js/main.js
@@ -10,8 +10,10 @@ const codeQuest = document.querySelector(".CodeQuest");
 const monitor = document.querySelector("#monitor");
 const blockMon = document.querySelector(".blockMon");
 const sel = document.querySelector("#sel");
+const nextBut = document.querySelector("#nextBut");
 const codeBlock = document.querySelector(".CodeBlock");
 const subCode = document.querySelector(".SubCode");
+const nextBtn = document.querySelector("#NextBtn");
 
 const deactivation = () => {
 	monitor.classList.remove('open');
@@ -20,11 +22,14 @@ const deactivation = () => {
 	subCode.classList.remove('hidden');
 	navMon.forEach(item => item.classList.remove('open'));
 	menu.forEach(item => item.classList.remove('open'));
+	nextBut.style.display = 'flex';
+	nextBtn.style.display = 'block';
+	codeQuest.textContent = '';
+	codeBlock.textContent = '';
 }
 
 blockBtn.forEach((btn,i)=>btn.addEventListener('click', () => {
 	deactivation();
-	sel.style.display = 'block';
 	nameQuest.textContent = block[i].textContent;
 	let quest = questLang(block[i].textContent);
 	textQuest.textContent = quest[0];
@@ -35,3 +40,29 @@ blockBtn.forEach((btn,i)=>btn.addEventListener('click', () => {
 	monitor.style.display = 'none';
 	sel.value = block[i].textContent;
 }));
+
+subCode.addEventListener('click', (e) => {
+	e.preventDefault();
+	codeBlock.textContent = "Проверяй сам себя!";
+});
+
+function select() {
+	deactivation();
+	nameQuest.textContent = sel.value;
+	let quest = questLang(sel.value);
+	textQuest.textContent = quest[0];
+	if(quest[1] === '')
+		blockMon.classList.add('hidden');
+	else
+		codeQuest.textContent = quest[1];
+}
+
+sel.addEventListener("change", () => {
+	select();
+	monitor.style.display = 'none';
+});
+
+nextBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	select();
+});
